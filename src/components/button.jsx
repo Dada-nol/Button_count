553bafdf2826847a35b69bc4f83c089d6d744aa0
Darren <nardol.darren@gmail.com>
2025-01-30 19:19:52 +0100

    create component Button

---
+++ src/components/button.jsx
@@ -0,0 +1,19 @@
+import { useState } from "react";
+
+function Button() {
+  const [count, setCount] = useState(0);
+
+  return (
+    <button
+      onClick={() => {
+        setCount((count) => {
+          count + 1;
+        });
+      }}
+    >
+      {count}
+    </button>
+  );
+}
+
+export default Button;
